d85768bd0b837b383fe1a2866fe404005e56877f
Andrey Matsovkin <macik.spb@gmail.com>
2012-10-05 17:10:17 +0400

    Some fixes for compatibility

--- plugins/extdev_showhide/extdev_showhide.rc.php
+++ plugins/extdev_showhide/extdev_showhide.rc.php
@@ -2,6 +2,7 @@
 /* ====================
 [BEGIN_COT_EXT]
 Hooks=rc
+Order=50
 [END_COT_EXT]
 ==================== */
 
@@ -18,16 +19,19 @@ if (!defined('COT_CODE') && !defined('COT_PLUG')) { die('Wrong URL ('.array_pop(
 
 $plug_name = 'extdev_showhide';
 
-if (defined('COT_ADMIN') && $_GET['n']=='edit' && $_GET['o']=='plug' && $_GET['m']=='config'
-				&& ($_GET['p']==$plug_name || $edh_cfg['track_mode'])) {
+if (!defined('EXTDEV_OFF') // other plugins can switch off this extension
+	&& defined('COT_ADMIN') && $_GET['n']=='edit' && $_GET['o']=='plug' && $_GET['m']=='config' // only on config page
+	&& ($_GET['p']==$plug_name || $edh_cfg['track_mode']))
+{
+
 	define('EXTDEV_SHOWHIDE',true);
 
 	global $edh_cfg;
 	$edh_cfg = $cfg['plugin']['extdev_showhide'];
+	// normalize suffixed param names
 	$edh_cfg = array_combine(preg_replace("/_#?sh\d*(_.*)?/", '', array_keys($edh_cfg)), array_values($edh_cfg));
 
 	if ($_GET['p']==$plug_name ) $adminhelp .= $L['edh_admin'];
-
 	cot_rc_link_footer($cfg['plugins_dir'].'/'.$plug_name.'/js/'.$plug_name.'.js');
 }
 
